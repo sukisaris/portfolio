7f4f3fb7c8d8fa95aa156148ec4cbba4067bc501
sukisaris <arissukiswanto2@gmail.com>
2020-09-21 09:46:31 +0700

    Updates

---
+++ precache-manifest.65214363f78ae3a9a3e9862e2bd2f1c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6472cc3cd6eb886c70da4414923612e4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "56eec910c7e472b53f5e",
+    "url": "/portfolio/static/css/main.a00a44f7.chunk.css"
+  },
+  {
+    "revision": "01cd93064f68dffc31f7",
+    "url": "/portfolio/static/js/2.ca664043.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.ca664043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56eec910c7e472b53f5e",
+    "url": "/portfolio/static/js/main.0e3e00bf.chunk.js"
+  },
+  {
+    "revision": "d1692e19eb03429b1f31",
+    "url": "/portfolio/static/js/runtime-main.6c29f35c.js"
+  },
+  {
+    "revision": "7375de8a20eb0918168969b21c1f5437",
+    "url": "/portfolio/static/media/coronainfo.7375de8a.png"
+  },
+  {
+    "revision": "d7a67ffe48a00cea16c4106608b8dc9c",
+    "url": "/portfolio/static/media/landingpage.d7a67ffe.png"
+  }
+]);
